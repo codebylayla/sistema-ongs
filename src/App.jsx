02cca9f36af2e7c0ce6d0f41dee7afbba1213331
Layla de Oliveira <layladeoliveira651@gmail.com>
2026-06-07 17:44:31 -0300

    Primeira versão do sistema ONGs

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container mt-5">
+
+      <h1 className="text-primary">
+        Meu primeiro React 🎉
+      </h1>
+
+      <p>
+        Finalmente funcionando.
+      </p>
+
+      <button className="btn btn-success">
+        Clique aqui
+      </button>
+
+    </div>
+  )
+}
+
+export default App
